420f582cd95021de0f6557beb7d5e5b7b370b36c
yashtripathi20005-creator <yashtripathi20005@gmail.com>
2026-06-20 15:37:13 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Minesweeper Clone - Main Entry Point
+Run this file to start the game.
+"""
+from src.game import Game
+
+
+def main():
+    game = Game()
+    game.run()
+
+
+if __name__ == "__main__":
+    main()
